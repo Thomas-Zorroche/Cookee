f73d5daa9e41755c0a269a5590b083975ee25199
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-02-04 19:55:28 +0100

    [form ingrd] css upgrades

--- src/ajoutIngrdPage.php
+++ src/ajoutIngrdPage.php
@@ -25,33 +25,46 @@ while($resultats = $req->fetch())
         <div id="Main-Window">
             <h1>Ajouter un Ingredient</h1>
 
-            <form method="post" action="cible.php">
             <div id="Form-cont">
-                <label>Nom de l'ingrédient (Français) : <input type="text" name="nom" /> </label>
-                
-                <div id="Ingrd-english-cont">
-                    <label>Nom de l'ingrédient (Anglais) : <input type="text" id="nom-english" name="nom-english" /> </label>
-                    <p class="btn-preview" onclick="displayIngrdThumbnail()">Preview</p>
+            <form method="post" action="cible.php">
+                <div class="form-group-cont">
+                    <h2>Nom de l'ingrédient (Fr)</h2>
+                    <div class="form-inputs-cont">
+                        <input type="text" name="nom" value="Sucre" />
+                    </div>
                 </div>
-                <a href="https://www.wordreference.com/"><p>wordreference.com</p></a>
-                <p>Sélectionner l'image appropriée pour l'ingrédient.</p>
 
-                <div id="ingrd-img-cont"></div>
+                <div class="form-group-cont">
+                    <h2>Nom de l'ingrédient (En)</h2>
+                    <div class="form-inputs-cont">
+                        <input type="text" id="nom-english" name="nom-english" value="Sugar" />
+                        <a href="https://www.wordreference.com/"><p>wordreference.com</p></a>
+                        <p class="btn-form" onclick="displayIngrdThumbnail()">Preview</p>
+                        <p>Sélectionner l'image appropriée pour l'ingrédient.</p>
+                    <div id="ingrd-img-cont"></div>
+                    </div>
+                </div>
+
+                <div class="form-group-cont">
+                    <h2>Unitée</h2>
+                    <div class="form-inputs-cont">
+                        <label>Unitée : 
+                        <select name="unite">
+                            <option value="Entree">ml</option>
+                            <option value="Plat">g</option>
+                            <option value="Dessert">c.a.c</option>
+                            <option value="Dessert">c.a.s</option>
+                            <option value="Dessert">sachet</option>
+                            <option value="Dessert">none</option>
+                        </select> 
+                        </label>
+                    </div>
+                </div>
                 
-                <label>Unitée : 
-                <select name="unite">
-                    <option value="Entree">ml</option>
-                    <option value="Plat">g</option>
-                    <option value="Dessert">c.a.c</option>
-                    <option value="Dessert">c.a.s</option>
-                    <option value="Dessert">sachet</option>
-                    <option value="Dessert">none</option>
-                </select> 
-                </label>
+                <input type="submit" id="Btn-validate" class="btn-form" value="Valider"  />
 
-                <input type="submit" value="Valider"  />
-            </div>
             </form>
+            </div>
 
         </div>
     
