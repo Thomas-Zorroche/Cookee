7d6ed757cb9e8f3822d7f5e61d5b15d70fdb9248
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-02-05 13:01:38 +0100

    [form ingrd] css upgrades buttons

--- src/ajoutIngrdPage.php
+++ src/ajoutIngrdPage.php
@@ -27,37 +27,33 @@ while($resultats = $req->fetch())
 
             <div id="Form-cont">
             <form method="post" action="cible.php">
-                <div class="form-group-cont">
+                <div class="form-windows-cont">
                     <h2>Nom de l'ingrédient (Fr)</h2>
                     <div class="form-inputs-cont">
                         <input type="text" name="nom" value="Sucre" />
                     </div>
                 </div>
 
-                <div class="form-group-cont">
+                <div class="form-windows-cont">
                     <h2>Nom de l'ingrédient (En)</h2>
                     <div class="form-inputs-cont">
-                        <input type="text" id="nom-english" name="nom-english" value="Sugar" />
-                        <a href="https://www.wordreference.com/"><p>wordreference.com</p></a>
-                        <p class="btn-form" onclick="displayIngrdThumbnail()">Preview</p>
-                        <p>Sélectionner l'image appropriée pour l'ingrédient.</p>
+                        <div id="Input-english-cont">
+                            <input type="text" id="nom-english" name="nom-english" value="Sugar" />
+                            <a href="https://www.wordreference.com/" target="_blank"><p>wordreference.com</p></a>
+                            <p class="btn-form" onclick="displayIngrdThumbnail()">Preview</p>
+                        </div>
+                        <p>Cliquer sur Preview puis sélectionner l'image appropriée pour l'ingrédient.</p>
                     <div id="ingrd-img-cont"></div>
                     </div>
                 </div>
 
-                <div class="form-group-cont">
+                <div class="form-windows-cont">
                     <h2>Unitée</h2>
-                    <div class="form-inputs-cont">
-                        <label>Unitée : 
-                        <select name="unite">
-                            <option value="Entree">ml</option>
-                            <option value="Plat">g</option>
-                            <option value="Dessert">c.a.c</option>
-                            <option value="Dessert">c.a.s</option>
-                            <option value="Dessert">sachet</option>
-                            <option value="Dessert">none</option>
-                        </select> 
-                        </label>
+                    <div class="form-inputs-cont" id="Radio-unite-cont">
+                        <div><input type="radio" name="unite" id="ml" value="ml"/> <label for="ml"> <span></span> ml </label> </div>
+                        <div><input type="radio" name="unite" id="g" value="g"/> <label for="g"> <span></span> g </label> </div>
+                        <div><input type="radio" name="unite" id="sachet" value="sachet"/> <label for="sachet"> <span></span> sachet </label> </div>
+                        <div><input type="radio" name="unite" id="verre" value="verre"/> <label for="verre"> <span></span> verre </label> </div>
                     </div>
                 </div>
                 
